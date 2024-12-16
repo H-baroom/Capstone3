a79867fa1e47994d22321a1860c830154d401a1c
Hashim Baroom <hashim.m.baroom@gmail.com>
2024-12-16 21:38:06 +0300

    Add files via upload

---
+++ InDTO/PurchaseInDTO.java
@@ -0,0 +1,13 @@
+package com.example.capstone3.InDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class PurchaseInDTO {
+    private Integer motorcycle_id;
+
+
+
+}
---
+++ InDTO/RentingRequestInDTO.java
@@ -0,0 +1,10 @@
+package com.example.capstone3.InDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RentingRequestInDTO {
+    private Integer renting_id;
+}
