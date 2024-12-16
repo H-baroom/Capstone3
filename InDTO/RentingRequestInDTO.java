c5093bc938ab8489c8667e2de8b8cff9eaa33dce
Hashim Baroom <hashim.m.baroom@gmail.com>
2024-12-16 21:43:57 +0300

    Update RentingRequestInDTO.java

--- InDTO/RentingRequestInDTO.java
+++ InDTO/RentingRequestInDTO.java
@@ -7,4 +7,12 @@ import lombok.Data;
 @AllArgsConstructor
 public class RentingRequestInDTO {
     private Integer renting_id;
+
+    @Column(columnDefinition = "date not null")
+    @FutureOrPresent(message = "Start date must be in the present or future")
+    private LocalDate startDate;
+
+    @Column(columnDefinition = "date not null")
+    @Future(message = "End date must be in the future")
+    private LocalDate endDate;
 }
